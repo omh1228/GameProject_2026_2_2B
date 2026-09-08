ef1e9811e56e9723c4d8369efa4279f4da14c78a
omh1228 <pigoh04>
2026-09-08 15:23:54 +0900

    임시 완성본 아님

---
+++ Assets/Script/BuildingType.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class BuildingType : MonoBehaviour
+{
+    public enum BuildingType
+    {
+        Restaurant,                         //음식점
+        Customer,                           //고객 집
+        ChargingStation                     //충전소 
+
+    }
+}
